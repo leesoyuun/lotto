fe277f18e65a4bfbb1bbefad041f5c3dc1bc2d24
leesoyun <leesoyun4206@naver.com>
2021-06-02 11:44:02 +0900

    hash code based on DatePicker

--- app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -28,6 +29,20 @@ class ConstellationActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+        val calendar = Calendar.getInstance()
+
+        datePicker.init(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH),
+                object: CalendarView.OnDateChangeListener,DatePicker.OnDateChangedListener{
+                    override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, dayOfMonth: Int)
+                    {}
+                    override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                        txtConstell.text=makeConstellationString(datePicker.month,datePicker.dayOfMonth)
+                    }
+
+                }
+
+
+        )
 
     }
     fun getShuffledLottoNumbersFromHash(str: String): MutableList<Int> {
@@ -37,7 +52,7 @@ class ConstellationActivity : AppCompatActivity() {
             list.add(number)
         }
 
-        val targetString = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SS", Locale.KOREA).format(Date())+str
+        val targetString = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date())+str
         list.shuffle(Random(targetString.hashCode().toLong()))
 
         return list.subList(0,6)
