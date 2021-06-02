c3d84c629486e36137999d815f5f0dde6714c726
leesoyun <leesoyun4206@naver.com>
2021-06-02 11:24:02 +0900

    Constellation-based hash code

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,14 +4,8 @@ package com.example.lotto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.cardview.widget.CardView
-import com.example.lotto.ConstellationActivity
-import com.example.lotto.NameActivity
-import com.example.lotto.R
-import com.example.lotto.ResultActivity
 import java.util.*
-import java.util.jar.Attributes
 import kotlin.collections.ArrayList
 
 fun getRandomLottoNumber () : Int{
@@ -66,7 +60,7 @@ fun getRandomLottoNumbers(): MutableList<Int> {
     }
     return lottoNumbers
 }
-fun getShuffledLottoNumbers () : MutableList<Int> {
+fun getShuffledLottoNumbers(toString: String): MutableList<Int> {
     val list = mutableListOf<Int>()
 
     for (number in 1..45) {
--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -3,13 +3,18 @@ package com.example.lotto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ResultActivity : AppCompatActivity() {
     //    val imageView1id = R.id.imageView1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        val result = intent.getIntegerArrayListExtra("result")// ?: return // ?: return
+        val result = intent.getIntegerArrayListExtra("result") ?: return
+        val sConstellation = intent.getStringExtra("constellation")
+
         //        // 2) sort the array
 //        result.sort()
 //        val result_sorted = result?.sortedBy{it}
@@ -19,8 +24,13 @@ class ResultActivity : AppCompatActivity() {
 //        result?.let{
 //            updateLottoBallImages(result.sortedBy{it})
 //        }
-        val lottoBallImageStartId = R.drawable.ball_01 // 146
-/*        val lottoBallImageId2 = R.drawable.ball_02 // 150
+        sConstellation?.let{
+            val resultLabel = findViewById<TextView>(R.id.resultLabel)
+            resultLabel.text = "${sConstellation}의 ${SimpleDateFormat("yyyy년 MM월 dd일").format(Date())} 로또 번호입니다."
+        }
+
+        // 146
+/*       val lottoBallImageId2 = R.drawable.ball_02 // 150
         val lottoBallImageId3 = R.drawable.ball_03 // 148*/
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
@@ -29,11 +39,16 @@ class ResultActivity : AppCompatActivity() {
         val imageView5 = findViewById<ImageView>(R.id.imageView5)
         val imageView6 = findViewById<ImageView>(R.id.imageView6)
 
-        imageView1.setImageResource(lottoBallImageStartId + result_sorted!![0] - 1)
+        val lottoBallImageStartId = R.drawable.ball_01
+        val lottoBallImageId2 = R.drawable.ball_02
+        val lottoBallImageId3 = R.drawable.ball_03
+
+        imageView1.setImageResource(lottoBallImageStartId + (result_sorted[0] - 1))
         imageView2.setImageResource(lottoBallImageStartId + result_sorted[1] - 1)
         imageView3.setImageResource(lottoBallImageStartId + result_sorted[2] - 1)
         imageView4.setImageResource(lottoBallImageStartId + result_sorted[3] - 1)
         imageView5.setImageResource(lottoBallImageStartId + result_sorted[4] - 1)
         imageView6.setImageResource(lottoBallImageStartId + result_sorted[5] - 1)
     }
+
 }
